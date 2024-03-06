da50769672882694da2247930a776f7fd7d1cdd2
KennyU13 <kennyurvano13@gmail.com>
2024-03-06 22:13:17 +0300

    fit aceuille

--- front_DevHunt/src/views/Acceuille.vue
+++ front_DevHunt/src/views/Acceuille.vue
@@ -2,25 +2,24 @@
      <section class="section landing pb-0">
     <div class="container p-3">
         
-        <div class="row justify-content-between align-items-center p-3">
+        <div class="row justify-content-between align-items-center p-3 m-3">
             <div class="col-lg-4 d-lg-block d-none">
                 <img src="../assets/images/eni.png" alt="illustration" class="img-fluid rounded" style="max-width: 90%">
             </div>
             <div class="col-lg-7  text-left">
                 <h1 class="mb-4">A propos des clubs</h1>
                 <p class="mb-5">Lorem ipsum dolor amet, consetetur sadiffspscing elitr, diam nonumy invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua At.</p>
-                
-                
+                 <button class="btn btn-primary">Voir</button>
             </div>
             
             
         </div>
-        <div class="row justify-content-between align-items-center p-3">
+        <div class="row justify-content-between align-items-center p-3 m-3">
             
             <div class="col-lg-7  text-left">
                 <h1 class="mb-4">A propos des mentors</h1>
                 <p class="mb-5">Lorem ipsum dolor amet, consetetur sadiffspscing elitr, diam nonumy invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua At.</p>
-                
+                <button class="btn btn-primary">Voir</button>
                 
             </div>
             <div class="col-lg-4 d-lg-block d-none">
@@ -29,30 +28,31 @@
             
             
         </div>
-        <div class="row justify-content-between align-items-center p-3">
+        <div class="row justify-content-between align-items-center p-3 m-3">
             <div class="col-lg-4 d-lg-block d-none">
                 <img src="../assets/images/eni.png" alt="illustration" class="img-fluid rounded" style="max-width: 90%">
             </div>
             <div class="col-lg-7  text-left">
                 <h1 class="mb-4">Vie Assosiative</h1>
-                <p class="mb-5">Lorem ipsum dolor amet, consetetur sadiffspscing elitr, diam nonumy invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua At.</p>
-                
+                <p class="mb-5">Lorem ipsum dolor amet, consetetur sadiffspscing elitr, diam nonumy invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua At. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias quis dolor iusto, nihil ipsum alias voluptatem autem consequatur officiis quibusdam modi ipsam? Maiores suscipit inventore dolores nulla mollitia voluptatum rerum!</p>
+                <button class="btn btn-primary">Voir</button>
                 
             </div>
             
             
         </div>
-        <div class="row justify-content-between align-items-center p-3">
+        <div class="row justify-content-between align-items-center p-3 m-3  ">
             
             <div class="col-lg-7  text-left">
-                <h1 class="mb-4">Vie Sportifs   </h1>
+                <h1 class="mb-4">Foire au questionnement</h1>
                 <p class="mb-5">Lorem ipsum dolor amet, consetetur sadiffspscing elitr, diam nonumy invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua At.</p>
-                
+                <button class="btn btn-primary">Voir</button>
                 
             </div>
             <div class="col-lg-4 d-lg-block d-none">
                 <img src="../assets/images/eni.png" alt="illustration" class="img-fluid rounded" style="max-width: 90%">
             </div>
+           
             
             
         </div>
@@ -66,5 +66,5 @@ export default {
 }
 </script>
 <style scoped>
-
+    
 </style>
