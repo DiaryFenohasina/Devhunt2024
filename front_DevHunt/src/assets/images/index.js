6c7c2f9bc828935e470dbac4b52b15e17ae0b291
NOOBIERAM <rotsiniainagg@gmail.com>
2024-03-07 04:24:08 +0300

    feat chat

--- front_DevHunt/src/assets/images/index.js
+++ front_DevHunt/src/assets/images/index.js
@@ -2,6 +2,9 @@ import banner from './banner.jpg'
 import basic from './basic.jpg'
 import cta from './cta.jpg'
 import logo from './logo.png'
+import bot from './bot.png'
+import chat from './chat.png'
+import mentor from './mentor.png'
 export {
-    banner,basic,cta,logo
+    banner,basic,cta,logo,bot,chat,mentor
 }
--- front_DevHunt/src/views/Discussion.vue
+++ front_DevHunt/src/views/Discussion.vue
@@ -3,49 +3,63 @@
     <div class="container shadow section-sm rounded min-Height">
       <div class="row">
         <div class="sideChat col-lg-1">
-            
-            <ul class="">
-                <li class="d-flex sidelist c-type" @click="selectPanel('ChatBot')">
-                    <i class="mb-4 ti-heart" :class="panel === 'ChatBot'? 'card-icon-Tools-active' : 'card-icon-Tools'"></i>
+            <ul>
+                <li class=" sidelist c-type" @click="selectPanel('ChatBot')" style="width=55px; height:55px">
+                    <img :src="bot" class="mb-4 p-2" :class="panel === 'ChatBot'? 'card-icon-Tools-active' : 'card-icon-Tools'" alt="">
                 </li>
-                <li class="d-flex sidelist c-type"  @click="selectPanel('Personne')">
-                    <i class="mb-4 ti-heart" :class="panel === 'Personne'? 'card-icon-Tools-active' : 'card-icon-Tools'"></i>
+                <li class=" sidelist c-type my-2"  @click="selectPanel('Personne')" style="width=55px; height:55px">
+                    <img :src="chat" class="mb-4 p-2" :class="panel === 'Personne'? 'card-icon-Tools-active' : 'card-icon-Tools' " >
                 </li>
-                <li class="d-flex sidelist c-type" @click="selectPanel('Mentor')">
-                    <i class="mb-4 ti-heart" :class="panel === 'Mentor'? 'card-icon-Tools-active' : 'card-icon-Tools'"></i>
-                </li>
-                <li class="d-flex sidelist c-type" @click="selectPanel('Mentor')">
-                    <i class="mb-4 ti-heart" :class="panel === 'Mentor'? 'card-icon-Tools-active' : 'card-icon-Tools'"></i>
+                <li class=" sidelist c-type" @click="selectPanel('Mentor')" style="width=55px; height:55px">
+                    <img :src="mentor" class="mb-4 p-2" :class="panel === 'Mentor'? 'card-icon-Tools-active' : 'card-icon-Tools'">
                 </li>
             </ul>
         </div>
         <div class="col-lg-4 px-4">
-            <h3 class="mb-5">Discussion</h3>
-            <div class="d-flex search-div mb-4 shadow-input ">
+            <h3 class="mb-5">{{panel}}</h3>
+            <div v-if="panel !=='ChatBot' ">
+                <div class="d-flex search-div mb-4 shadow-input ">
                 <input type="text" class="ps-3 flex-grow-1 search-input me-auto text-left">
                 <button type="submit" class="search-btn pe-3"><i class="ti-search h5"></i></button>
             </div>
-          <ul class="p-0" v-for="user in users" :key="user.id">
-            <div>
-                <li class="d-flex user-li c-type" :class="index == user.id && 'isActive'" @click="selectUser(user.id)">
-                    {{user.username}}<div v-if="isOnline(user.id)" class="online"></div>
-                </li>
+          <ul class="p-2" :class="index == user.id ? 'rounded-user-msg-active' : 'rounded-user-msg'" v-for="user in users" :key="user.id">
+            <div class="d-flex align-items-center c-type user-msg" @click="selectUser(user.id)">
+                <div><img :src="picture" alt="Avatar" class=" rounded-circle avatar-msg" ></div>
+                <div class="mx-3 flex-grow-1 text-left">
+                    <li class="user-msg" >{{user.username}}</li>
+                    <li class="me-auto text-light">Message</li>
+                </div>
+                <div v-if="isOnline(user.id)" class="online"></div>
             </div>
           </ul>
+            </div>
         </div>
-        <div class="col-lg-7">
-          <div class="mx-5 message">
+        <div class="col-lg-7 align-items-center">
+          <div class="me-5 message">
 
                <!-- USER TO CHAT -->
-              
-                <div v-if="user.username">
-                  <div  class="rounded-5 shadow p-2 mb-4 " style="height:50px">
-                    <h4 class="d-flex ">{{user.username}} <span class="mx-4 p-0" >connected</span></h4>  
+              <!--  -->
+                <div v-if="user.username ? user.username : true ">
+                  <div v-if="panel === 'ChatBot'" class="rounded-5 d-flex align-items-center shadow p-2 mb-4 " style="height:50px">
+                    <div ><img :src="bot" alt="Avatar" class=" rounded-circle avatar-msg-clicked" ></div><strong class="ms-3 d-flex ">Assistance IA</strong>  
+                  </div>
+                  <div v-else class="rounded-5 d-flex align-items-center shadow p-2 mb-4 " style="height:50px">
+                    <div ><img :src="picture" alt="Avatar" class=" rounded-circle avatar-msg-clicked" ></div><strong class="ms-3 d-flex ">{{user.username}}</strong>  
                   </div>
 
                   <!-- CHAT MESSAGE -->
                   <div class=" rounded-5 mb-3 bg-light ">
-                    <ul  class="px-3 card-message  list-group  rounded-5 ">
+                    <ul v-if="panel === 'ChatBot'" class="px-3 card-message  list-group rounded-5 " v-chat-scroll="{always: false, smooth: true}">
+                      
+                      <li v-for="(message,i) in messagesChat" :key="i" :class="{'mt-4' : index == 0}" >
+                        <p class=" message-font shadow" :class="[ message.type !== 'bot' ? 'float-end chat-l' : 'chat-r'  ]">
+                          {{ message.msg }}
+                          <!-- <small>:{{ message.id}}</small>  -->
+                          
+                        </p>
+                      </li>
+                    </ul>
+                    <ul v-else class="px-3 card-message  list-group rounded-5 " v-chat-scroll="{always: false, smooth: true}">
                       
                       <li v-for="(message, index) in messages" :key="index" :class="{'mt-4' : index == 0}" >
                         <p class=" message-font shadow" :class="[ message.UserId == id ? 'float-end chat-l' : 'chat-r'  ]">
@@ -55,14 +69,14 @@
                         </p>
                       </li>
                     </ul>
-                    <div class="search-wrapper  d-sm-flex">
-                      <input id="search-by" name="s" type="email" class="form-control form-control-sm" placeholder="Your message here" style="width:100%" v-model="newMessage" @keypress.enter="sendMessage">
-                      <input type="submit" class="btn btn-primary btn-sm" value="Send" @click="sendMessage">
+                    <div class="search-wrapper align-items-center d-sm-flex " style="background:#fff;">
+                        <input id="search-by" name="s" type="text" class="form-control form-control-sm" placeholder="Your message here" style="width:100%; background:transparent" v-model="newMessage" @keypress.enter="sendMessage">
+                        <input type="submit" class="btn btn-primary btn-sm" value="Send" @click="sendMessage">
                     </div>
                   </div>
                 </div>
 
-
+                <!--  -->
               <div v-else class="text-center">
                 <p  class="message" >Choose an user to chat</p>
               </div>
@@ -76,41 +90,58 @@
 <script>
 import { backServer } from "@/config/axiosConfig";
 import { io }  from "socket.io-client"
+import sary from '@/assets/images/avatar.png'
+import {bot,chat, mentor} from '@/assets/images/index'
 
 export default {
   data() {
     return {
+        bot,chat,mentor,
+        picture: sary,
       index: -1,
-      id: parseInt(localStorage.getItem("id")),
-      username: localStorage.getItem("username"),
+      id : 1,
+      username : 'Rotsy',
+    //   id: parseInt(localStorage.getItem("id")),
+    //   username: localStorage.getItem("username"),
       panel:'ChatBot',
       user:{},
       room : [],
       users: [
-        {
-            username: 'RAMANANTSOA Rotsiniaina'
-        },
-        {
-            username: 'RATSIMBAZAFY Tojoniaina Mbola Safidy'
-        }
+        // {
+        //     id: 0,
+        //     username: 'RAMANANTSOA Rotsiniaina'
+        // },
+        // {
+        //     id: 1,
+        //     username: 'RATSIMBAZAFY Tojoniaina Mbola Safidy'
+        // }
 
         ],
       connectedUsers: [],
       newMessage: null,
-      messages: [],
+      messages: [
+        // {
+        //     UserId: 1,
+        //     msg : 'hello'
+        // },
+        // {
+        //     UserId: 0,
+        //     msg : 'hello'
+        // }
+      ],
+      messagesChat:[],
       socket: null
       
     };
   },
   created() {
-    // this.socketConnect()
-    // this.getUsers()
+    
+    this.socketConnect()
+    this.getUsers()
   },
-  updated(){
-  },  
   beforeDestroy()
   {
-    //   this.disconnect()
+      this.disconnect()
   },
   methods: {
     isOnline(index)
@@ -125,6 +156,11 @@ export default {
     selectPanel(x)
     {
         this.panel = x
+        if(x == 'ChatBot')
+        {
+            
+            this.joinRoom()
+        }
     },
     getUsers() {
       backServer
@@ -146,7 +182,7 @@ export default {
     },
     socketConnect(){
       
-      // socket : io.connect("http://192.168.43.19:4001")
+    //   socket : io.connect("http://192.168.43.19:4001")
 
       this.socket = io("http://127.0.0.1:5000")
       this.socket.emit('status',{username: this.username, UserId:this.id})
@@ -155,7 +191,14 @@ export default {
       })
     },
     joinRoom(anotherID){
-      this.socket.emit('join', `${this.id}--with--${anotherID}`)
+      if(this.panel == 'ChatBot')
+      {
+        this.socket.emit('join', 'bot')
+      }
+      else{
+        this.socket.emit('join', `${this.id}--with--${anotherID}`)
+      }
+    //   this.socket.emit('join', `${this.id}--with--${anotherID}`)
     },
     getMessages(){
       this.messages = []
@@ -174,18 +217,43 @@ export default {
     {
       if(!this.newMessage) return
 
-      let msg = {UserId : this.id, msg: this.newMessage, room: this.room}
+      if(this.panel === 'ChatBot')
+      {
+        let msg = { msg: this.newMessage, type: 'user'}
+        this.messagesChat = [...this.messagesChat, msg]
+        backServer.post('/chat',{message: this.newMessage})
+        .then(async res => {
+        //   if(res.data.message == 'message stored'){
+            const message = await res.data.msg
+            // console.log(res.data.msg)
+            this.socket.emit('sendMessage', message)
+            // console.log(res.data.message)
+            this.messagesChat = [...this.messagesChat, message]
+            // console.log(this.messages)
+        //   }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        
+        this.newMessage = ''
+      }
+      else{
+        let msg = {UserId : this.id, msg: this.newMessage, room: this.room}
         backServer.post('/sendMessage',msg)
         .then(res => {
           if(res.data.message == 'message stored'){
+            // console.log(res.data.msg);
             this.socket.emit('sendMessage', res.data.msg)
             this.messages = [...this.messages, res.data.msg]
+            // console.log(this.messages)
           }
         })
         .catch(err => {
           console.log(err)
         })
       this.newMessage = ''
+      }
 
     },
     setRoom(id)
@@ -260,9 +328,6 @@ export default {
         
     }
     .online{
-        position: relative;
-        left: 15px;
-        top: 20px;
         width: 8px;
         height: 8px;
         border-radius:50px;
@@ -274,10 +339,8 @@ export default {
     .active{
         color: var(--baseColor);
     }
-    li:hover{
-        color: var(--baseColor);
-    }
     .card-icon-Tools-active {
+
         background: var(--baseColor);
         color: #fff;
         height: 55px;
@@ -328,5 +391,28 @@ export default {
         background: transparent;
         border: 0;
     }
+    .shadow-input{
+        box-shadow: 10px 9px 60px rgba(62, 62, 62, 0.102) !important;
+    }
+    .avatar-msg{
+        max-width: 50px;
+        min-width: 50px;
+    }
+    .avatar-msg-clicked{
+        max-width: 40px;
+        min-width: 40px;
+    }
+    .user-message{
+        position: relative;
+        margin-left: 1cm;
+    }
+    .rounded-user-msg-active{
+        background: #1cdd4c69;
+        color: #000;
+        border-radius: 12px !important;
+    }
+    .rounded-user-msg:hover{
+        background: #1cdd4c69;
+        border-radius: 12px !important;
+    }
 </style>
-</template>
